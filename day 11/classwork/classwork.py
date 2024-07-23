8494b7dc8a83ae40a2b570116bfc8557b8014957
123avtuka123 <tati444kurtanidze@mail.ru>
2024-07-23 13:34:21 +0400

    (commit)

---
+++ day 11/classwork/classwork.py
@@ -0,0 +1,38 @@
+steps=int (input("input your steps: "))
+
+active_minutes=int (input("input your active minutes: "))
+
+needed_steps=int (10000)
+
+needed_minutes=int (30)
+
+if steps > needed_steps:
+    print("true")
+else:
+    print("false")
+
+if active_minutes > needed_minutes:
+    print("true")
+else:
+    print("false")
+
+
+
+
+
+
+
+
+
+
+
+
+your_age=int (input("input your age: "))
+
+needed_age___my_age=int (11)
+
+if your_age == needed_age___my_age:
+    print("true")
+else:
+    print("false")
+
---
+++ day 11/homework/homework.py
@@ -0,0 +1,72 @@
+name=str (input("input your name: "))
+
+my_name=str ("avtuka")
+
+if name == my_name:
+    print("true")
+else:
+    print("false")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+your_num=int (input("input your num: "))
+
+needed_num=int (10)
+
+if your_num <= needed_num:
+    print("true")
+else:
+    print("false")
+
+
+
+
+print( )
+print( )
+print( )
+print( )
+print( )
+print( )
+
+
+
+
+
+
+
+
+print(93<97)
+print(47<74)
+print(45<85)
+print(35<78)
+print(37>95)
+print(74>26)
+print(23>75)
+print(24>26)
+print(24<=66)
+print(22<=22)
+print(24<=45)
+print(24<=73)
+print(35>=36)
+print(54>=73)
+print(37>=36)
+print(27>=37)
+print(33==33)
+print(87==87)
+print(15==83)
+print(48==24)
+print(72==24)
