3cce577790889e7f0d6d82be35e2d2f099c09a3e
123avtuka123 <tati444kurtanidze@mail.ru>
2024-08-20 10:20:01 +0400

    (commit)

---
+++ day 16/classwork/classwork.py
@@ -0,0 +1,22 @@
+for i in range(0, 10 + 1, 2):
+    print(i)
+
+
+your_nums = int(input("Enter your nums:"))
+
+for i in range( 1 , your_nums + 1):
+    print(i)
+
+
+print( )
+print( )
+print( )
+print( )
+print( )
+print( )
+print( )
+print( )
+
+for i in range(30):
+    print(i)
+    
---
+++ day 16/homework/homework.py
@@ -0,0 +1,16 @@
+for i in range(0, 100 + 2, 2):
+    print(i)
+
+print( ) 
+print( ) 
+print( ) 
+print( ) 
+
+
+
+for i in range(1, 100, 2):
+    print(i)
+
+
+
+
